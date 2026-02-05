d690f5a1a08e7504d30c95e7897aaffb64d158f5
Mironc <shafikovamirka@gmail.com>
2026-02-05 17:35:27 +0500

    add main workspace

---
+++ examples/simple.rs
@@ -0,0 +1,5 @@
+use graphics;
+//Empty for now
+pub fn main(){
+    game_engine::print(game_engine::add(1,3));
+}
---
+++ src/lib.rs
@@ -0,0 +1,6 @@
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+pub fn print(num:u64){
+    println!("{}",num);
+}
