57554563e64b768e5a9158787264d1a3eb3b3992
Mironc <shafikovamirka@gmail.com>
2026-02-07 02:00:49 +0500

    feat(texture): implement TextureView and its storage
    - add TextureView and TextureViewCreate structs
    - implement storage in TextureContainer
    - update documentation and naming

--- crates/graphics/src/rendering/texture_container.rs
+++ crates/graphics/src/rendering/texture_container.rs
@@ -1,8 +1,8 @@
 use std::{collections::HashMap, error::Error};
 
 use ash::vk::{
-    Extent3D, Format, Image, ImageCreateInfo, ImageLayout, ImageTiling, ImageType, ImageUsageFlags,
-    ImageView, SampleCountFlags,
+    Extent3D, Format, Image, ImageAspectFlags, ImageCreateInfo, ImageLayout, ImageSubresourceRange,
+    ImageTiling, ImageType, ImageUsageFlags, ImageView, ImageViewCreateInfo, SampleCountFlags,
 };
 use gpu_allocator::{
     MemoryLocation,
@@ -12,10 +12,10 @@ use slotmap::{SlotMap, new_key_type};
 
 use crate::device::DeviceContext;
 
-/// 
+/// Centralized container for managing all that related to textures, but not array of textures
 pub struct TextureContainer {
     images: SlotMap<TextureId, Texture>,
-    image_views: SlotMap<TextureViewId, ImageView>,
+    image_views: SlotMap<TextureViewId, TextureView>,
     image_view_to_image: HashMap<TextureViewId, TextureId>,
 }
 impl TextureContainer {
@@ -27,25 +27,26 @@ impl TextureContainer {
             image_view_to_image: HashMap::new(),
         }
     }
+
     /// Creates `Texture` with given `CreateTexture`
-    /// 
+    ///
     /// # Errors
-    /// returns error if image creation or memory allocation fails 
+    /// returns error if image creation or memory allocation fails
     pub fn create_texture(
         &mut self,
         device: &DeviceContext,
         create: CreateTexture,
     ) -> Result<TextureId, Box<dyn Error>> {
-        let image_type = if create.extent.depth > 1 {
+        let image_type = if create.dimensions.depth > 1 {
             ImageType::TYPE_3D
-        } else if create.extent.height > 1 {
+        } else if create.dimensions.height > 1 {
             ImageType::TYPE_2D
         } else {
             ImageType::TYPE_1D
         };
         //TODO:Add more parameters to create texture
         let image_create = ImageCreateInfo::default()
-            .extent(create.extent)
+            .extent(create.dimensions)
             .image_type(image_type)
             .initial_layout(ImageLayout::UNDEFINED)
             .format(create.image_format)
@@ -73,15 +74,52 @@ impl TextureContainer {
         let texture = Texture {
             alloc,
             image,
-            extent: create.extent,
+            extent: create.dimensions,
             image_type,
             format: create.image_format,
         };
         Ok(self.images.insert(texture))
     }
-    /// Creates `TextureView` with given `CreateTexture` 
-    pub fn create_texture_view(&mut self, create: CreateTextureView) -> TextureViewId {
-        self.image_views.insert(ImageView::null())
+
+    /// Creates `TextureView` with given `CreateTexture`
+    pub fn create_texture_view(
+        &mut self,
+        device: &DeviceContext,
+        create: CreateTextureView,
+    ) -> Result<TextureViewId, Box<dyn Error>> {
+        if let Some(texture_id) = create.texture_id {
+            let subresource = ImageSubresourceRange::default()
+                .base_mip_level(0)
+                .aspect_mask(ImageAspectFlags::COLOR)
+                .base_array_layer(0)
+                .layer_count(1);
+            let image_view_createinfo = ImageViewCreateInfo::default()
+                .format(create.view_format)
+                .image(self.images[texture_id].image)
+                .subresource_range(subresource);
+            let texture = self.get_image(texture_id).unwrap();
+            let image_view = unsafe { device.create_image_view(&image_view_createinfo, None) }?;
+            let texture_view = TextureView{ image_view, extent: texture.dimensions(), format: create.view_format };
+            let view_id = self.image_views.insert(texture_view);
+
+            self.image_view_to_image.insert(view_id, texture_id);
+            return Ok(view_id);
+        }
+        Err("No TextureId was provided".into())
+    }
+
+    /// Returns a reference to the `Texture` associated with the `TextureId`
+    ///
+    /// Returns `None` if texture has been destroyed or the `TextureId` is invalid
+    pub fn get_image(&self, texture_id: TextureId) -> Option<&Texture> {
+        self.images.get(texture_id)
+    }
+
+    /// Returns a reference to the `TextureView` associated with the `TextureViewId`
+    ///
+    /// Returns `None` if texture has been destroyed or the `TextureViewId` is invalid
+    pub fn get_image_view(&self, view_id: TextureViewId) -> Option<&TextureView> {
+        self.image_views.get(view_id)
     }
 
     /// **FOR TESTING PURPOSES**
@@ -91,15 +129,17 @@ impl TextureContainer {
     pub fn create_texture_null(&mut self) -> TextureId {
         self.images.insert(Texture::default())
     }
+
     /// **FOR TESTING PURPOSES**
     ///
     /// Creates a dummy `TextureView`
     #[cfg(test)]
     pub fn create_texture_view_null(&mut self) -> TextureViewId {
-        self.image_views.insert(ImageView::null())
+        self.image_views.insert(TextureView::default())
     }
 }
 
+//TODO:Add reference counting for automatic dispose
 new_key_type! {
     /// Unique identifier to a `Texture` in a `TextureContainer`
     pub struct TextureId;
@@ -110,7 +150,7 @@ new_key_type! {
     pub struct TextureViewId;
 }
 
-/// Texture
+/// A GPU-driven image resource.
 #[derive(Debug, Default)]
 pub struct Texture {
     image: Image,
@@ -118,11 +158,38 @@ pub struct Texture {
     image_type: ImageType,
     extent: Extent3D,
     format: Format,
+    //TODO:Add mipmap level count
+}
+impl Texture {
+    /// Returns the raw handle `VKImage`
+    pub fn handle(&self) -> Image {
+        self.image
+    }
+
+    /// Returns the physical dimensions of texture
+    pub fn dimensions(&self) -> Extent3D {
+        self.extent
+    }
+
+    /// Returns the underlying image `Format`
+    pub fn format(&self) -> Format {
+        self.format
+    }
+
+    /// Returns the dimensionality type of the texture (1D, 2D, or 3D).
+    pub fn image_type(&self) -> ImageType {
+        self.image_type
+    }
+
+    /// Returns the reference to the allocation info of texture
+    pub fn allocation(&self) -> &Allocation {
+        &self.alloc
+    }
 }
 /// Configuration parameters for creating a `Texture`.
 #[derive(Debug, Clone, Default)]
 pub struct CreateTexture {
-    extent: Extent3D,
+    dimensions: Extent3D,
     image_format: Format,
 }
 impl CreateTexture {
@@ -133,7 +200,7 @@ impl CreateTexture {
 
     /// Sets the dimensions of the texture
     pub fn dimensions(mut self, width: u32, height: u32, depth: u32) -> Self {
-        self.extent = Extent3D {
+        self.dimensions = Extent3D {
             width,
             height,
             depth,
@@ -147,8 +214,28 @@ impl CreateTexture {
         self
     }
 }
-//TODO:Fill in
+
+/// A struct that define how renderer will read and write data into `Texture`
+#[derive(Debug,Default)]
+pub struct TextureView {
+    image_view: ImageView,
+    extent: Extent3D,
+    format: Format,
+    //TODO:Add mipmap level
+}
 /// Configuration parameters for creating a `TextureView`.
+#[derive(Debug, Clone, Default)]
 pub struct CreateTextureView {
-    texture_id: TextureId,
+    texture_id: Option<TextureId>,
+    view_format: Format,
+}
+impl CreateTextureView {
+    pub fn new() -> Self {
+        Self::default()
+    }
+    /// Sets `TextureId` for the image view
+    pub fn texture_id(mut self, texture_id: TextureId) -> Self {
+        self.texture_id = Some(texture_id);
+        self
+    }
 }
